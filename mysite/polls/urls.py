18c2bca60a1fae7538849b3d703820cd9fdeec91
wanderson-gomes <82663603+wanderson-gomes@users.noreply.github.com>
2021-06-08 18:28:59 -0300

    finale commit
    finale adjusts and tests.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,5 +6,13 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('pag1/', views.pag1, name='pag1'),
 	path('pag2/', views.pag2, name='pag2'),
+	path('pag3/', views.pag3, name='pag3'),
+	path('pag4/', views.pag4, name='pag4'),
+	path('pag5/', views.pag5, name='pag5'),
+	path('pag6/', views.pag6, name='pag6'),
+	path('pag7/', views.pag7, name='pag7'),
+	path('pag8/', views.pag8, name='pag8'),
+	path('pag9/', views.pag9, name='pag9'),
+	path('pag10/', views.pag10, name='pag10'),
 	path('about/', views.about, name='about'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -74,3 +74,148 @@ def pag2(request):
 		'title': "Noticia 2"
 	}
 	return render(request, "polls/pag2.html", context)
+
+def pag3(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 3"
+	}
+	return render(request, "polls/pag3.html", context)
+
+def pag4(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 4"
+	}
+	return render(request, "polls/pag4.html", context)
+
+def pag5(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 5"
+	}
+	return render(request, "polls/pag5.html", context)
+
+def pag6(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 6"
+	}
+	return render(request, "polls/pag6.html", context)
+
+	
+def pag7(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 7"
+	}
+	return render(request, "polls/pag7.html", context)
+
+def pag8(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 8"
+	}
+	return render(request, "polls/pag8.html", context)
+
+def pag9(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 9"
+	}
+	return render(request, "polls/pag9.html", context)
+
+def pag10(request):
+	if request.method == 'POST':
+		mensagem = "Comentário Registrado\n"
+		mensagem += "Nome: "+request.POST['nome']+"\n"
+		mensagem += "Email: "+request.POST['email']+"\n"
+		mensagem += "Mensagem: "+request.POST['mensagem']+"\n"
+		send_mail(
+			'Seu comentário em 127.0.0.1:8000 foi registrado com sucesso!',
+			mensagem,
+			'from@example.com',
+			['wanderson.helo@gmail.com'],
+			fail_silently=False,
+			)
+	context = {
+		'title': "Noticia 10"
+	}
+	return render(request, "polls/pag10.html", context)
